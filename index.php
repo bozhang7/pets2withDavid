bc48193da78e14560a2d290599e3ed24613fab74
Bo Zhang <bzhang3@mail.greenriver.edu>
2019-06-11 03:37:16 -0400

    Added route for form 2 and the results page.

--- index.php
+++ index.php
@@ -44,12 +44,25 @@
         echo $view->render('home.html');
     });
 
-
     //define route for order 1
-    $f3->route('GET /order', function ()
-    {
-        $view=new Template();
+    $f3->route('GET /order', function () {
+        $view = new Template();
         echo $view->render( 'views/form1.html');
+    });
+
+    //define route for order 2
+    $f3->route('GET|POST /order2', function () {
+        $_SESSION['animal'] = $_POST['animal'];
+
+        $view = new Template();
+        echo $view->render( 'views/form2.html');
+    });
+
+    $f3->route('GET|POST /results', function() {
+        $_SESSION['color'] = $_POST['color'];
+
+        $view = new Template();
+        echo $view->render( 'views/results.html');
     });
 
     //Run fat free
